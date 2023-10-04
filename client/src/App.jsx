3dd00931d6fbbde394b452b2d9259e4fb01cfeea
NattyNgrn <natnrgn@gmail.com>
2023-10-04 13:33:46 -0400

    making components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import UserLogin from './components/userLogin'
 
 function App() {
 
   return (
     <div>
-
+      <UserLogin> </UserLogin>
     </div>
   )
 };
--- client/src/components/userLogin.jsx
+++ client/src/components/userLogin.jsx
@@ -67,4 +67,6 @@ function UserLogin(){
       </div>
     </div>
     )
-}
+};
+
+export default UserLogin;
